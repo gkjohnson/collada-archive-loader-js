5caf9bf8b8bb8887d93b91323e64489678ce13a7
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-06-15 10:55:13 -0700

    doobify, add linters

--- ColladaArchiveLoader.js
+++ ColladaArchiveLoader.js
@@ -5,135 +5,134 @@
 
 THREE.ColladaArchiveLoader = function ( manager ) {
 
-    this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
-    this._colladaLoader = new THREE.ColladaLoader();
+	this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
+	this._colladaLoader = new THREE.ColladaLoader();
 
-}
+};
 
 THREE.ColladaArchiveLoader.prototype = {
 
-    constructor: THREE.ColladaArchiveLoader,
+	constructor: THREE.ColladaArchiveLoader,
 
-    load: function ( url, onLoad, onProgress, onError ) {
+	load: function ( url, onLoad, onProgress, onError ) {
 
-        var scope = this;
+		var scope = this;
 
-        var path = scope.path === undefined ? THREE.LoaderUtils.extractUrlBase( url ) : scope.path;
+		var loader = new THREE.FileLoader( this.manager );
+		loader.setResponseType( 'arraybuffer' );
+		loader.load( url, function ( data ) {
 
-        var loader = new THREE.FileLoader( this.manager );
-        loader.setResponseType( 'arraybuffer' );
-        loader.load( url, function ( data ) {
+			onLoad( scope.parse( data ) );
 
-            onLoad( scope.parse( data ) );
+		}, onProgress, onError );
 
-        }, onProgress, onError );
+	},
 
-    },
+	parse: function ( data ) {
 
-    parse: function( data ) {
+		function cleanPath( path ) {
 
-        function cleanPath( path ) {
+			if ( /^file:/.test( path ) ) {
 
-            if ( /^file:/.test( path ) ) {
+				console.warn( 'ColladaArchiveLoader : file:// URI not supported.' );
+				return '';
 
-                console.warn( 'ColladaArchiveLoader : file:// URI not supported.' );
-                return '';
+			}
 
-            }
+			path = path.replace( /\\+/g, '/' );
+			path = path.replace( /^\.?\//, '' );
 
-            path = path.replace( /\\+/g, '/' );
-            path = path.replace( /^\.?\//, '' );
+			var spl = path.split( '/' );
+			var newpath = [];
 
-            var spl = path.split( '/' );
-            var newpath = [];
+			while ( spl.length !== 0 ) {
 
-            while ( spl.length !== 0 ) {
+				var token = spl.shift();
 
-                var token = spl.shift();
+				if ( token === '..' ) newpath.pop();
+				else newpath.push( token );
 
-                if ( token === '..' ) newpath.pop();
-                else newpath.push( token );
+			}
 
-            }
+			return newpath.join( '/' );
 
-            return newpath.join( '/' );
-        }
+		}
 
-        if ( window.JSZip == null ) {
+		if ( window.JSZip == null ) {
 
-            console.error( 'ColladaArchiveLoader : JSZip is required to unpack a Collada archive file.' );
-            return null;
+			console.error( 'ColladaArchiveLoader : JSZip is required to unpack a Collada archive file.' );
+			return null;
 
-        }
+		}
 
-        try {
+		try {
 
-            var zip = new JSZip(data);
+			var zip = new JSZip( data );
 
-            // Find the entry file
-            var manifest = zip.file( 'manifest.xml' ).asText();
-            var entryfile;
+			// Find the entry file
+			var manifest = zip.file( 'manifest.xml' ).asText();
+			var entryfile;
 
-            if ( manifest == null ) {
+			if ( manifest == null ) {
 
-                var files = zip.file( /\.dae$/i );
+				var files = zip.file( /\.dae$/i );
 
-                if ( files.length === 0 ) {
+				if ( files.length === 0 ) {
 
-                    console.error( 'ColladaArchiveLoader : No manifest found and no Collada file found to load.' );
+					console.error( 'ColladaArchiveLoader : No manifest found and no Collada file found to load.' );
 
-                }
+				}
 
-                if ( files.length >= 2 ) {
+				if ( files.length >= 2 ) {
 
-                    console.error( 'ColladaArchiveLoader : No manifest found and more than one Collada file found to load.' );
+					console.error( 'ColladaArchiveLoader : No manifest found and more than one Collada file found to load.' );
 
-                }
+				}
 
-                entryfile = files[0].name;
+				entryfile = files[ 0 ].name;
 
-            } else {
+			} else {
 
-                var manifestxml = manifest && (new DOMParser()).parseFromString( manifest, 'application/xml' );
-                entryfile = [ ...manifestxml.children ]
-                    .filter( c => c.tagName === 'dae_root' )[0]
-                    .textContent
-                    .trim();
+				var manifestxml = manifest && ( new DOMParser() ).parseFromString( manifest, 'application/xml' );
+				entryfile = [ ...manifestxml.children ]
+					.filter( c => c.tagName === 'dae_root' )[ 0 ]
+					.textContent
+					.trim();
 
-            }
+			}
 
-            entryfile = cleanPath( entryfile );
+			entryfile = cleanPath( entryfile );
 
-            // get the dae file and directory
-            var dir = entryfile.replace ( /[^\/]*$/g, '' );
-            var daefile = zip.file( entryfile ).asText();
+			// get the dae file and directory
+			var dir = entryfile.replace( /[^\/]*$/g, '' );
+			var daefile = zip.file( entryfile ).asText();
 
-            // parse the result
-            var result = this._colladaLoader.parse( daefile );
+			// parse the result
+			var result = this._colladaLoader.parse( daefile );
 
-            for ( var name in result.images ) {
-                
-                var image = result.images[ name ];
-                var path = decodeURI( image.init_from );
-                var texpath = cleanPath( `${ dir }/${ cleanPath(image) }` );
-                var data = zip.file( texpath ).asArrayBuffer();
-                var blob = new Blob( [ data ], 'application/octet-binary' );
-                image.build.src = URL.createObjectURL( blob );
+			for ( var name in result.images ) {
 
-            }
+				var image = result.images[ name ];
+				var path = decodeURI( image.init_from );
+				var texpath = cleanPath( `${ dir }/${ cleanPath( image ) }` );
+				var data = zip.file( texpath ).asArrayBuffer();
+				var blob = new Blob( [ data ], 'application/octet-binary' );
+				image.build.src = URL.createObjectURL( blob );
 
-            // return the data
-            return result;
+			}
 
-        } catch ( e ) {
+			// return the data
+			return result;
 
-            console.error( 'ColladaArchiveLoader : The Collada archive file is not valid.' );
-            console.error( e );
+		} catch ( e ) {
 
-            return null;
+			console.error( 'ColladaArchiveLoader : The Collada archive file is not valid.' );
+			console.error( e );
 
-        }
+			return null;
 
-    }
+		}
 
-}
+	}
+
+};
